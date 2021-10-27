fc3c2c66cdffc42cb474a48c0c19021612f0cfa4
fiyahraa <luthfiyahza19@gmail.com>
2021-10-27 19:34:06 +0700

    menambahkan file program

---
+++ Main.py
@@ -0,0 +1,42 @@
+#Biodata Mahasiswa 
+
+print("### Tugas Pemrograman ###")
+print("nama :Luthfiyah")
+print("NIM :312110131")
+print("Tema :Program penginputan biodata")
+
+print("###############")
+print("###############")
+print("###############")
+
+#Pembuka
+print ("Assalamualaikum")
+print ("Let me introduce my self")
+
+#nama
+data = input("My Name Is:")
+print("Nama Lengkap = ",data,)
+
+#nama panggilan
+data = input("But you can call me:")
+print("Nama Panggilan = ",data,)
+
+#NPM
+data = input("My NPM Is:")
+print("npm=",data)
+
+#Tempat Tanggal lahir
+data = input("I Was Born in:")
+print("Tempat lahir = ",data,)
+
+#Umur
+data_int = int(input("umur: "))
+print("Alamat =",data)
+
+#Alamat
+data = input ("iam very glad if you want to invite my house in:")
+print("Alamat =",data)
+
+#No Telepon
+data_int = int(input("so, dont forget to call me before with the number"))
+print("no telepon:" ,data_int)
